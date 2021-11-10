6436cf409313bead3f406bf6b337d1f41103e53c
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2021-11-10 09:53:09 -0800

    homework 4
    it be homework 4

---
+++ Week10/Assets/Scripts/SimpleNavMeshController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class SimpleNavMeshController : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _agent;
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private ThirdPersonCharacter character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+
+        if (_agent.remainingDistance > _agent.stoppingDistance)
+        {
+            character.Move(_agent.desiredVelocity, false, false);
+        } else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+       
+    }
+}
